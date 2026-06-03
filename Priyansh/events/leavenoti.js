9064aeecb07e1189ea34f7d34fa9b43ee14aa46f
teachnicalsatyamji1290-web <teachnicalsatyamji1290@gmail.com>
2026-06-03 07:20:16 +0530

    Update credits and welcome message in leavenoti.js

--- Priyansh/events/leavenoti.js
+++ Priyansh/events/leavenoti.js
@@ -2,7 +2,7 @@ module.exports.config = {
         name: "leave",
         eventType: ["log:unsubscribe"],
         version: "1.0.0",
-        credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
+        credits: "Satyam pandey",
         description: "Notify the Bot or the person leaving the group with a random gif/photo/video",
         dependencies: {
                 "fs-extra": "",
@@ -40,7 +40,7 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 
         if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-(typeof data.customLeave == "undefined") ? msg = "💐𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 𝐒𝐇𝐀𝐀𝐍 𝐁𝐎𝐓 𝐌𝐄𝐑𝐈 𝐉𝐀𝐀𝐍💐😇👈\n──────────────\n\n {name} \n\n──────────────\n𝐊𝐎 𝐁𝐇𝐆𝐀 𝐃𝐈𝐘𝐀🌝 \n──────────────────\n {type} \n──────────────\n𝐎𝐰𝐧𝐞𝐫 ➻  ──── 💐 𝐒𝐇𝐀𝐀𝐍 💐\n──────────────\n {name} \n──────────────\n 💐💐𝐊𝐈𝐓𝐍𝐀 𝐂𝐔𝐓𝐄 𝐓𝐇𝐀 𝐍𝐀 𝐘𝐄𝐇💐💐😥 ✨✨ 𝐆𝐑𝐎𝐔𝐏 𝐒𝐄 𝐂𝐇𝐀𝐋𝐀 𝐆𝐀𝐘𝐀 ♥ 𝐀𝐁 𝐌𝐄𝐑𝐀 𝐈𝐒𝐊𝐄 𝐁𝐈𝐍𝐀 𝐊𝐀𝐈𝐒𝐄 𝐌𝐀𝐍 𝐋𝐀𝐆𝐄𝐆𝐀🤔🤔\n──────────────\n\n[❤️‍🔥] 🖤🖤😥😥...Good {session} || {time}" : msg = data.customLeave;
+(typeof data.customLeave == "undefined") ? msg = "💐𝐖𝐄𝐋𝐂𝐎𝐌𝐄 𝐓𝐎 Satyam pandey 𝐁𝐎𝐓 𝐌𝐄𝐑𝐈 𝐉𝐀𝐀𝐍💐😇👈\n──────────────\n\n {name} \n\n──────────────\n𝐊𝐎 𝐁𝐇𝐆𝐀 𝐃𝐈𝐘𝐀🌝 \n──────────────────\n {type} \n──────────────\n𝐎𝐰𝐧𝐞𝐫 ➻  ──── 💐 Satyam pandey 💐\n──────────────\n {name} \n──────────────\n 💐💐𝐊𝐈𝐓𝐍𝐀 𝐂𝐔𝐓𝐄 𝐓𝐇𝐀 𝐍𝐀 𝐘𝐄𝐇💐💐😥 ✨✨ 𝐆𝐑𝐎𝐔𝐏 𝐒𝐄 𝐂𝐇𝐀𝐋𝐀 𝐆𝐀𝐘𝐀 ♥ 𝐀𝐁 𝐌𝐄𝐑𝐀 𝐈𝐒𝐊𝐄 𝐁𝐈𝐍𝐀 𝐊𝐀𝐈𝐒𝐄 𝐌𝐀𝐍 𝐋𝐀𝐆𝐄𝐆𝐀🤔🤔\n──────────────\n\n[❤️‍🔥] 🖤🖤😥😥...Good {session} || {time}" : msg = data.customLeave;
         msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type).replace(/\{session}/g, hours <= 10 ? "𝙈𝙤𝙧𝙣𝙞𝙣𝙜" : 
     hours > 10 && hours <= 12 ? "𝘼𝙛𝙩𝙚𝙧𝙉𝙤𝙤𝙣" :
     hours > 12 && hours <= 18 ? "𝙀𝙫𝙚𝙣𝙞𝙣𝙜" : "𝙉𝙞𝙜𝙝𝙩").replace(/\{time}/g, time);  
